5c61c7c84a39915207fb85e34cd0d2cc1d1ae8e9
Zhanglaplace <lapcace@gmail.com>
2017-05-31 18:10:32 +0800

    add tree iterms
    Signed-off-by: Zhanglaplace <lapcace@gmail.com>

---
+++ Tree/102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getDepth(TreeNode* root){
+        if(root == nullptr)
+            return 0;
+        return max(getDepth(root->left),getDepth(root->right))+1;
+    }
+    
+    void getValue(vector<vector <int>>& res,TreeNode* root,int level){
+        if(root == nullptr)
+            return ;
+        res[level].push_back(root->val);
+        getValue(res,root->left,level+1);
+        getValue(res,root->right,level+1);
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>res;
+        int depth = getDepth(root);
+        if(depth == 0)
+            return res;
+        res.resize(depth);
+        getValue(res,root,0);
+        return res;
+    }
+};
---
+++ Tree/103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getDepth(TreeNode* root){
+        if(root == nullptr)
+            return 0;
+        return max(getDepth(root->left),getDepth(root->right))+1;
+    }
+    
+    void getValue(vector<vector <int>>& res,TreeNode* root,int level){
+        if(root == nullptr)
+            return ;
+        res[level].push_back(root->val);
+        getValue(res,root->left,level+1);
+        getValue(res,root->right,level+1);
+    }
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>res;
+        int depth = getDepth(root);
+        if(depth == 0)
+            return res;
+        res.resize(depth);
+        getValue(res,root,0);
+        for(int i = 1 ;i < res.size();i+=2){
+            reverse(res[i].begin(),res[i].end());
+        }
+        return res;
+    }
+};
