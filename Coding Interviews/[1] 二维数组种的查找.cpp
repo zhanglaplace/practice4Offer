9b3a1eccb6c1a1f124e0c3fa27a554c54140b187
Zhanglaplace <lapcace@gmail.com>
2017-06-03 23:00:04 +0800

    Signed-off-by: Zhanglaplace <lapcace@gmail.com>

---
+++ Coding Interviews/[1] 二维数组种的查找.cpp
@@ -0,0 +1,24 @@
+/******************
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，
+每一列都按照从上到下递增的顺序排序。请完成一个函数，
+输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+******************/
+class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        if(array.size() == 0)
+            return false;
+        int m = array.size();
+        int n = array[0].size();
+        int i = 0,j = n-1;
+        while(i < m && j >= 0){
+            if(array[i][j] == target)
+                return true;
+            else if(array[i][j] > target)
+                j--;
+            else
+                i++;
+        }
+        return false;
+    }
+};
